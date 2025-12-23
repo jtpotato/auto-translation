49d21784844741a09ad2b151a6c1ffde7271939a
jtpotato <58995538+jtpotato@users.noreply.github.com>
2025-12-23 14:14:04 +1100

    add translation ability

--- index.ts
+++ index.ts
@@ -1,1 +1,36 @@
-console.log("Hello via Bun!");
+import { Client, GatewayIntentBits } from "discord.js";
+import OpenAI from "openai";
+
+const client = new Client({
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.MessageContent,
+  ],
+});
+
+const openai = new OpenAI({
+  baseURL: "https://api.openai.com/v1",
+  apiKey: process.env.OPENAI_KEY,
+});
+
+client.once("clientReady", () => {
+  console.log(`Logged in as ${client.user?.tag}!`);
+});
+
+client.on("messageCreate", async (message) => {
+  if (message.author.bot) return;
+  const result = await openai.responses.create({
+    model: "gpt-5-nano",
+    input:
+      "You are a translator. If the text is in English, translate it to Mandarin Chinese. If the text is in Mandarin Chinese, translate it to English.Only output the translated message. Text:\n\n" +
+      message.content,
+    reasoning: {
+      effort: "minimal",
+    },
+  });
+
+  message.reply(result.output_text);
+});
+
+client.login(process.env.BOT_TOKEN);
